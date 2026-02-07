331e60200e7098c368ba5e034697b377e27c32bb
StarDust130 <csyadav0513@gmail.com>
2026-02-07 13:53:02 +0530

    feat: integrate Clerk for authentication and add user sign-in/sign-up functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,32 @@
 import { ModeToggle } from "@/components/ui/ModeToggle";
 
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-     <ModeToggle />
+      <header className="flex justify-end items-center p-4 gap-4 h-16">
+        {/* Show the sign-in and sign-up buttons when the user is signed out */}
+        <SignedOut>
+          <SignInButton />
+          <SignUpButton>
+            <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+              Sign Up
+            </button>
+          </SignUpButton>
+        </SignedOut>
+        {/* Show the user button when the user is signed in */}
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
+      <ModeToggle />
     </div>
   );
 }
